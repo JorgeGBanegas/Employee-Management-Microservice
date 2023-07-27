4ea24b71274d60781b875a1d9662982790ab581e
Jorge Banegas <jorge.g.banegas@gmail.com>
2023-07-26 21:36:34 -0400

    chore: Configurar conexion de la base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { TypeOrmModule} from "@nestjs/typeorm";
+import { ConfigModule } from '@nestjs/config';
+import { DataSourceConfig } from './config/data.sources';
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(
+      {
+        envFilePath: '.env',
+        isGlobal: true,
+      }
+    ),
+    TypeOrmModule.forRoot({...DataSourceConfig}),
+    
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import * as morgan from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(8002);
+
+  const configService = app.get(ConfigService);
+  console.log("🚀 ~ file: main.ts:9 ~ bootstrap ~ configService:", configService.get('PORT'))  
+  app.use(morgan('dev'));
+  await app.listen(configService.get('PORT'));
 }
 bootstrap();
